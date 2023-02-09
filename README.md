397fe9c385770ccaea68ccc08ada577ef77f6f2c
dgacosta <diego.garcia.acosta@gmail.com>
2023-02-09 01:22:08 +0100

    initial commit
    Proyecto generado con Spring Initializr 3.0.2. Dependencias:
    - Spring Web

---
+++ README.md
@@ -0,0 +1,5 @@
+# spring-rest-api-playground
+Proyecto playground para el curso "Building a REST API with Spring Boot" de Spring Academy.
+
+* Cursos: https://spring.academy/courses
+* **"Building a REST API with Spring Boot"**: https://spring.academy/courses/building-a-rest-api-with-spring-boot
---
+++ src/main/java/com/playground/application/SpringRestApiPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.playground.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiPlaygroundApplication.class, args);
+	}
+
+}
